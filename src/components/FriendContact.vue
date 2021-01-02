27ce90b3a20df2d057ff14546693ee2767be22eb
Alessandro Mondelli <alexs98@hotmail.it>
2021-01-02 16:57:30 +0100

    Creazione di una rubrica con il framework Vue.js

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,70 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? '(Favorito)' : ''}}</h2>
+    <button @click="toggleFavorite">Inserisci/Togli preferito</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Dettagli</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Telefono:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
+  props: { //Props con validazione
+    id: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    emailAddress: {
+      type: String,
+      required: true
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  emits: ['toggle-favorite'],
+  // emits: { //emits con validazione
+  //   'toggle-favorite': function(id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn('Id is missing!');
+  //       return false;
+  //     }
+  //   } 
+  // },
+  data() {
+    return {
+      detailsAreVisible: false
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit('toggle-favorite', this.id);
+    },
+  },
+};
+</script>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,56 @@
+<template>
+    <form @submit.prevent="submitData"> <!--Evito alla pagina di ricaricarsi e richiamo una funzione -->
+        <div class="form-group">
+            <label>Nome</label>
+            <input type="text" placeholder="Nome" v-model="enteredName" />
+        </div>
+       <div class="form-group">
+            <label>Telefono</label>
+            <input type="tel" placeholder="Telefono" v-model="enteredPhone"/>
+        </div>
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" placeholder="Email" v-model="enteredEmail"/>
+        </div>
+        <div class="form-group">
+            <button>Aggiungi Contatto</button>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    emits: ['add-contact'],
+    data() {
+        return {
+            enteredName: '',
+            enteredPhone: '',
+            enteredEmail: '',
+        }
+    },
+    methods: {
+        submitData() {
+            if( ( this.enteredName.length != 0 ) && ( this.enteredPhone.length != 0 ) && ( this.enteredEmail.length != 0 )  ) {
+                this.$emit('add-contact', this.enteredName, this.enteredPhone, this.enteredEmail) //Invio dati presi dal form ad App.vue
+
+                this.enteredName = '';
+                this.enteredPhone = '';
+                this.enteredEmail = '';
+            } else {
+                alert("All fields are required!");
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    .form-group {
+        margin-bottom: 20px;     
+    }
+
+    .form-group label {
+        margin-right: 10px;
+        text-align: left;
+    }
+</style>
